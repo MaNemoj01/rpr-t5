2bec7d8b43711a0f3bdcc8f87c1c5b2c7a3602d2
MaNemoj01 <44163914+MaNemoj01@users.noreply.github.com>
2018-11-12 18:02:12 +0100

    popravka i dodavaja metoda za onClick

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,26 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+
 public class Controller {
+    private SimpleStringProperty username;
+    brojevi = new SimpleStringProperty("");
+
+    public void btnBrojClick(ActionEvent actionEvent) {
+    }
+
+    public void btnOperacijaClick(ActionEvent actionEvent) {
+    }
+
+    public void btnEnterClick(ActionEvent actionEvent) {
+    }
+
+    public SimpleStringProperty usernameProperty() {
+        return brojevi;
+    }
+    public String getBrojevi() {
+        return brojevi.get();
+    }
 
 }
